ebd32eb9202b35d08846d17a51585355976ddaad
Devanshu07 <devasrivastava21@gmail.com>
2025-02-14 01:59:31 +0530

    Trying to set redis protocol

--- protocol.go
+++ protocol.go
@@ -1,8 +1,38 @@
 package main
 
-type Command struct {
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
 
+	"github.com/tidwall/resp"
+)
+const {
+    CommandSET
 }
-func parseCommand(msg []byte) (Command, error) {
-	
+type Command interface {
+}
+
+type SetCommand struct {
+	key, val string
+}
+
+func parseCommand(raw string) (Command, error) {
+	rd := resp.NewReader(bytes.NewBufferString(raw))
+	for {
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+        if v.Type() == resp.Array {
+            for i, v := range v.Array() {
+                fmt.Println(" #%d %s, value: '%s'\n", i, v.Type(), v)
+            }
+        }
+	}
+    return "foo", nil
 }
